052b922d9632f64e5b4ada92d0a208b8a9835b5e
ayusefi <a.yusefi1991@gmail.com>
2025-06-20 18:07:31 +0300

    Update PointNetClassifier forward to return trans_feat for regularization

--- model.py
+++ model.py
@@ -61,24 +61,25 @@ class PointNetClassifier(nn.Module):
         self.dropout = nn.Dropout(p=0.3)
 
     def forward(self, x):
-        # x: (batch_size, N, 3)
         x = x.transpose(2, 1)  # (batch_size, 3, N)
 
         trans = self.input_transform(x)
         x = torch.bmm(trans, x)
-
         x = F.relu(self.bn1(self.conv1(x)))
 
         trans_feat = self.feature_transform(x)
         x = torch.bmm(trans_feat, x)
 
         x = F.relu(self.bn2(self.conv2(x)))
-        x = self.bn3(self.conv3(x))  # (batch_size, 1024, N)
+        x = self.bn3(self.conv3(x))
 
-        x = torch.max(x, 2)[0]  # (batch_size, 1024)
+        x = torch.max(x, 2)[0]
 
         x = F.relu(self.bn4(self.fc1(x)))
         x = F.relu(self.bn5(self.dropout(self.fc2(x))))
         x = self.fc3(x)
+
+        return F.log_softmax(x, dim=1), trans_feat
+
 
         return F.log_softmax(x, dim=1)
